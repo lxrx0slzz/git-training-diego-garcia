b2bc7709beb5bca6aed46501babe35c5b0567c32
lxrx0slzz14 <diegogval10@gmail.com>
2023-11-03 18:25:13 +0100

    Fase1

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+	public static void  main(String[] args) {
+	  
+	 Producto producto1 = new Producto("Ordenador", 1000);
+	 Producto producto2 = new Producto("Movil", 750);
+
+	 EncargoProductos = new EncargoProductos();
+
+	 encargo.agregarProductos(producto1);
+	 encargo.agregarProductos(producto2);
+
+	 gestor.mostrarInformacionProductos();
+	}
+}
